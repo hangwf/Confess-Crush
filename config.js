705329eb2047771e59007b557d85628bd32d80ae
hangwf <94388671+hangwf@users.noreply.github.com>
2021-11-16 00:31:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     introDesc: `Chưa bao giờ em nghĩ rằng anh là gu của em đâu
     Chưa bao giờ em nghĩ rằng em will fall in love with you(dịch cho Minh hong biếc là rơi vào lưới tình🙄🙄)
     Mà giọng nói ngọt ngào cất ra từ headphone
-    Say đắm cồn cào lỡ yêu rồi mất khôn🙄🙄,
+    Say đắm cồn cào lỡ yêu rồi mất khôn🙄🙄',
     btnIntro: 'Híhí:33',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên :33',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy',
@@ -16,7 +16,7 @@ const CONFIG = {
     mess: 'Yêu Minh nhiêu nhắm nhắm nhuôn ó :33',
     messDesc: 'Minh dỗi Minh buồng Minh cáu xì mềnh vẫn yêu Minhhh :33,
     nhưng mò hong muốn xấy Minh buồng với suy nghỉ nhiều đouuu🥺🥺hong chỉ buồn bé rồi suy nghỉ mà bất cứ chuyện gì củng vậy mềnh hongg muốn xấy Minhh buồn như xế 🥺🥺
-    buồng xíu xíu hoiii nhó :(( có bé ở bên Minhh mò 🥺🥺 ôm ôm xương xương nào ❤️❤️.',
+    buồng xíu xíu hoiii nhó :(( có bé ở bên Minhh mò 🥺🥺 ôm ôm xương xương nào ❤️❤️',
     btnAccept: 'xem xongg gòiii xì call ngay cho tuii đi xôi :33',
     messLink: 'https://www.facebook.com/messages/t/100032861798311' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
